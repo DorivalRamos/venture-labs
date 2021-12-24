0dcef3e3079a0b19de01de2a67a2081b8cf04341
DorivalRamos <dorivalmillan2@hotmail.com>
2021-12-24 11:02:52 -0300

    Iniciando o Projeto e criando a Conexão com o BD

---
+++ src/configs/typeorm.config.ts
@@ -0,0 +1,14 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const typeOrmConfig: TypeOrmModuleOptions = {
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: 'root',
+  password: 'Dodor@mos26',
+  database: 'VentureLabs',
+  logging: true,
+  entities: [__dirname + '/../**/*.entity.{js,ts}'],
+  synchronize: true,
+
+};
