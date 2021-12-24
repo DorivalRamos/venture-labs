177d75caaac30ad5c53a6a44882b8d0e515b593d
DorivalRamos <dorivalmillan2@hotmail.com>
2021-12-24 13:24:46 -0300

    Fix: atualização de dados da Companhias Dto

--- src/companhias/dto/create-companhia.dto.ts
+++ src/companhias/dto/create-companhia.dto.ts
@@ -1,17 +1,9 @@
-import {
-  IsString,
-  IsEmail,
-  IsNotEmpty,
-  MaxLength,
-  MinLength,
-  Matches,
-  IsOptional,
-} from 'class-validator';
+import { IsString, IsNotEmpty, MaxLength, MinLength } from 'class-validator';
 
 export class CreateCompanhiaDto {
   @IsString()
   @IsNotEmpty({ message: 'Informe um nome da companhia valido' })
-  @MinLength(4, { message: 'O nome da Campania dev ter menos de 200 carters ' })
+  @MinLength(4, { message: 'O nome da Campania deve ter mais de 4 carters ' })
   @MaxLength(200, {
     message: 'O nome da companhia deve ter menos de 200 caracteres',
   })
